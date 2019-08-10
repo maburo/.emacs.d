6ea7d213f57562b615a05f28b5150bb9c9b4eb4b
Maburo <galex.rus@gmail.com>
2019-08-10 13:17:50 +0300

    wip

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (skeletor docker-compose-mode company-c-headers workgroups2 ranger helpful cider clojure-mode visual-regexp visual-replace undo-tree anzu try company-tern flycheck git-timemachine yasnippet-snippets use-package))))
+    (drag-stuff smart-hungry-delete emmet-mode skeletor docker-compose-mode company-c-headers workgroups2 ranger helpful cider clojure-mode visual-regexp visual-replace undo-tree anzu try company-tern flycheck git-timemachine yasnippet-snippets use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
