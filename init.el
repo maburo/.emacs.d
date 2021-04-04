76d89d4ec433c954627ad7bd484eff651a3e32c3
maburo <galex.rus@gmail.com>
2021-04-04 11:46:38 +0300

    wip

---
+++ init.el
@@ -0,0 +1,16 @@
+(let ((gc-cons-threshold most-positive-fixnum))
+  (require 'org)
+  (org-babel-load-file
+   (expand-file-name "config.org" user-emacs-directory)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
